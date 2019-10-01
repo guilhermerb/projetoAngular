c89c286036897361c1560d6831df6ae7faf3c262
Guilherme <Guilherme@DESKTOP-0BV0RV7>
2019-10-01 13:22:49 -0300

    PrimeiroCommit

---
+++ src/app/MyFirst.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+  selector: 'myFirst',
+  templateUrl: './MyFirst.component.html',
+  styleUrls: ['./MyFirst.component.css']
+})
+
+export class MyFirstAppComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstAppComponent } from './MyFirst.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstAppComponent
   ],
   imports: [
     BrowserModule,
